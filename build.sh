654866a353a9e814b00ad987bb4953eb34273ee7
Thierry Thuon <lepiaf@users.noreply.github.com>
2017-09-01 11:33:06 +0200

    Script build.sh

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+rm hosts*
+wget https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling/hosts
+cat hosts | grep '^0\.0\.0\.0' | awk '{print "local-zone: \""$2"\" redirect\nlocal-data: \""$2" A 0.0.0.0\""}' > ads.conf
+sudo cp ads.conf /etc/unbound/unbound.conf.d/ads.conf
+sudo service unbound reload

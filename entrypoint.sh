8ca601e59ef2fa0c59bba47de844abc089506d95
Thierry Thuon <thierry@thuon.eu>
2018-01-04 13:14:45 +0100

    create dockerfile

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+RECIPE=${RECIPE:-'fakenews-gambling'}
+
+rm hosts*
+wget https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/$RECIPE/hosts
+cat hosts | grep '^0\.0\.0\.0' | awk '{print "local-zone: \""$2"\" redirect\nlocal-data: \""$2" A 0.0.0.0\""}' > output.conf
+cat output.conf

be49b2dd5f6e02a9e7831a8bb04e751216f999dc
micadeyeye <micadeyeye@gmail.com>
2018-03-16 14:29:35 +0800

    Update build.sh - edit dir path for unbound conf
    The default dir path in Freebsd is /etc/unbound/conf.d/

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 rm hosts*
 wget https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling/hosts
 cat hosts | grep '^0\.0\.0\.0' | awk '{print "server:\nlocal-zone: \""$2"\" redirect\nlocal-data: \""$2" A 0.0.0.0\""}' > ads.conf
-sudo cp ads.conf /etc/unbound/unbound.conf.d/ads.conf
+sudo cp ads.conf /etc/unbound/conf.d/ads.conf
 sudo service unbound reload

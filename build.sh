6ce87161a8c597241fc15c419f89a8ffca92d3ab
Michael A. Oshin <micadeyeye@gmail.com>
2018-03-16 08:36:26 +0000

    the server: should be inserted once at the top

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 rm hosts*
 wget https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling/hosts
-cat hosts | grep '^0\.0\.0\.0' | awk '{print "server:\nlocal-zone: \""$2"\" redirect\nlocal-data: \""$2" A 0.0.0.0\""}' > ads.conf
+cat hosts | grep '^0\.0\.0\.0' | awk '{print "local-zone: \""$2"\" redirect\nlocal-data: \""$2" A 0.0.0.0\""}' > ads.conf
+echo 'server: ' | cat - ads.conf > temp && mv temp ads.conf
 sudo cp ads.conf /etc/unbound/conf.d/ads.conf
 sudo service local_unbound restart

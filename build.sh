3f4cae35131aef00abb2f6c8a5341299175b853f
Michael A. Oshin <micadeyeye@gmail.com>
2018-03-16 07:05:27 +0000

    Preferred to use local_unbound in FBSD & run restart than reload even though gen warnings

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ rm hosts*
 wget https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling/hosts
 cat hosts | grep '^0\.0\.0\.0' | awk '{print "server:\nlocal-zone: \""$2"\" redirect\nlocal-data: \""$2" A 0.0.0.0\""}' > ads.conf
 sudo cp ads.conf /etc/unbound/conf.d/ads.conf
-sudo service unbound reload
+sudo service local_unbound restart

17b98c287509f6b266a3eaadee20ef214d6d1421
Michael A. Oshin <micadeyeye@gmail.com>
2018-04-19 13:52:32 +0000

    Selected the recipe: fakenews-gambling-porn-social

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm hosts*
-wget https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling/hosts
+wget https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn-social/hosts
 cat hosts | grep '^0\.0\.0\.0' | awk '{print "local-zone: \""$2"\" redirect\nlocal-data: \""$2" A 0.0.0.0\""}' > ads.conf
 echo 'server: ' | cat - ads.conf > temp && mv temp ads.conf
 sudo cp ads.conf /etc/unbound/conf.d/ads.conf
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-RECIPE=${RECIPE:-'fakenews-gambling'}
+RECIPE=${RECIPE:-'fakenews-gambling-porn-social'}
 
 wget --quiet https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/$RECIPE/hosts
 cat hosts | grep '^0\.0\.0\.0' | awk '{print "local-zone: \""$2"\" redirect\nlocal-data: \""$2" A 0.0.0.0\""}' > output.conf
